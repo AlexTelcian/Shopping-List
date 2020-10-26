8867c068a3b95d75ead1a1be4c2edc80879b4979
AlexTelcian <alexandru.telcian99@e-uvt.ro>
2020-10-26 09:15:33 +0200

    Shopping List app

---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, ShoppingList.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.java
@@ -0,0 +1,74 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ShoppingList extends AppCompatActivity {
+
+    private ListView shoppingList;
+    private EditText nameItem;
+    private Button addItem,clearItem;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> list;
+    private String item;
+    private int index=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        Methods.onAttachedWindows(ShoppingList.this);
+        final TinyDB Save=new TinyDB(ShoppingList.this);
+
+        shoppingList = findViewById(R.id.shopping_listView);
+        nameItem = findViewById(R.id.item_editText);
+        addItem = findViewById(R.id.addButton);
+        clearItem = findViewById(R.id.clearButton);
+
+        list = Save.getListString("shoppingList");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,list);
+        shoppingList.setAdapter(adapter);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nameItem.length() == 0 )
+                    nameItem.setHint("empty");
+                else
+                 {
+                    item = nameItem.getText().toString();
+                    index = Save.getInt("index");
+                    list.add(index + " - " + item);
+                    index++;
+                    Save.putInt("index", index);
+                    Save.putListString("shoppingList", list);
+                    adapter.notifyDataSetChanged();
+                    nameItem.setText("");
+                    nameItem.setHint("Name item");
+                 }
+            }
+        });
+        clearItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Save.remove("shoppingList");
+                Save.putInt("index",1);
+                adapter.clear();
+
+            }
+        });
+
+
+    }
+}
